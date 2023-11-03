3920ad31a36356526e004f533403aebefc1a3a68
Sebastian <sebastian@eiweleit.de>
2023-11-03 22:01:40 +0100

    Update to version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# var-debug
-Its my varDebug Function
+# Extended version of var_dump
+
+Sometimes you need to know what's inside a variable, but you don't want to use var_dump() because it's too verbose or whatever. This function is a better alternative. Just pass 
+it a variable and it will return a page with the variable's contents in a readable format.
+
+## Usage
+
+```php
+<?php
+
+// get autoloader
+require_once 'vendor/autoload.php';
+
+// use the function
+dump($variable);
+```
+
+## Installation
+
+```bash
+composer require --dev basteyy/vardump
+```
--- src/varDebug.php
+++ src/varDebug.php
@@ -1,20 +1,68 @@
-<?php declare(strict_types=1);
+<?php
+/**
+ * @author Sebastian Eiweleit <sebastian@eiweleit.de>
+ * @website https://github.com/basteyy
+ * @website https://eiweleit.de
+ */
+
+declare(strict_types=1);
+
+use JetBrains\PhpStorm\NoReturn;
 
 if (!function_exists('varDebug')) {
+
     /**
-     * varDebug is just a simple var_dump on steroids
-     * @param ...$data
+    * Dump all passed variables and exit the script. Used a styled output. Accordion inspired by Raúl Barrera
+    * @param ...$mixed
+    * @see https://codepen.io/raubaca/pen/PZzpVe
      */
-    function varDebug(...$data)
+    #[NoReturn] function varDebug(...$mixed): void
     {
-        ob_end_clean();
-        echo '<pre>';
-        foreach ($data as $item) {
-            echo '<code>';
-            var_dump($item);
-            echo '</code><hr />';
+        ob_start();
+        $cache_output = function ($item) {
+            ob_start();
+            try {
+                var_dump($item);
+                $data = ob_get_clean();
+                return is_string($data) ? (htmlentities($data)) : 'nodata' . $data;
+            } catch (\Throwable $ex) {
+                // PHP8 ArgumentCountError for 0 arguments, probably..
+                // in php<8 this was just a warning
+                ob_end_clean();
+                throw $ex;
+            }
+        };
+
+        $x = 0;
+        $data_collection = '';
+        foreach ($mixed as $item) {
+            $x++;
+            $xx = time() . $x;
+            $data_collection .= '<div><input type="checkbox" id="_debug_' . $xx . '" checked /><label for="_debug_' . $xx . '">#' . $x .
+                ' <span class="google" data-debug="debug_' . $xx . '">Google</span></label> <pre><code id="debug_' . $xx . '">';
+            $data_collection .= $cache_output($item);
+            $data_collection .= '</code></pre></div>';
         }
-        echo '</pre>';
-        die();
+
+        $environmental = ['SERVER' => $_SERVER, 'POST' => $_POST, 'GET' => $_GET, 'REQUEST' => $_REQUEST];
+
+        foreach ($environmental as $env => $values) {
+            $data_collection .= '<div id="' . $env . '"><input type="checkbox" id="_' . $env . '"><label for="_' . $env . '">$_' . $env . '</label> <pre><code><table>';
+
+            foreach ($values as $name => $content) {
+                $data_collection .= sprintf('<tr><td class="title"><span class="copyme">$_%s[\'%s\']</span> </td> <td>=&gt;</td>  <td><pre class="in-table"><code>%s</code></pre></td></tr>',
+                    $env,
+                    $name,
+                    $cache_output
+                    ($content));
+            }
+
+            // $data_collection .= $cache_output();
+            $data_collection .= '</table></code></pre></div>' . PHP_EOL;
+        }
+        http_response_code(503);
+        ob_clean();
+        include __DIR__ . DIRECTORY_SEPARATOR . 'template.php';
+        exit();
     }
 }
